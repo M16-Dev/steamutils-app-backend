7ccafd2bbaaf29ab9c01a35b719028fe01ffb598
Kameq <90651206+kameqdev@users.noreply.github.com>
2026-02-02 19:33:26 +0100

    refactor: move app initialization and routing to app.ts

---
+++ app.ts
@@ -0,0 +1,30 @@
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { HTTPException } from "hono/http-exception";
+
+import codesRouter from "./routes/api/v1/codes.ts";
+import connectionsApiRouter from "./routes/api/v1/connections.ts";
+import connectRouter from "./routes/connect.ts";
+import connectionsRouter from "./routes/connections.ts";
+
+const app = new Hono();
+
+app.use(logger());
+
+const routes = app
+  .route("/api/v1/codes", codesRouter)
+  .route("/api/v1/connections", connectionsApiRouter)
+  .route("/connect", connectRouter)
+  .route("/connections", connectionsRouter);
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
+  }
+
+  console.error("Internal Server Error:", err);
+  return c.json({ error: "Internal Server Error" }, 500);
+});
+
+export default app;
+export type AppType = typeof routes;
--- main.ts
+++ main.ts
@@ -1,32 +1,4 @@
-import { Hono } from "hono";
-import { logger } from "hono/logger";
-import { HTTPException } from "hono/http-exception";
 import { config } from "./config.ts";
-
-import codesRouter from "./routes/api/v1/codes.ts";
-import connectionsApiRouter from "./routes/api/v1/connections.ts";
-import connectRouter from "./routes/connect.ts";
-import connectionsRouter from "./routes/connections.ts";
-
-const app = new Hono();
-
-app.use(logger());
-
-const routes = app
-  .route("/api/v1/codes", codesRouter)
-  .route("/api/v1/connections", connectionsApiRouter)
-  .route("/connect", connectRouter)
-  .route("/connections", connectionsRouter);
-
-export type AppType = typeof routes;
-
-app.onError((err, c) => {
-  if (err instanceof HTTPException) {
-    return err.getResponse();
-  }
-
-  console.error("Internal Server Error:", err);
-  return c.json({ error: "Internal Server Error" }, 500);
-});
+import app from "./app.ts";
 
 Deno.serve({ port: config.port }, app.fetch);
