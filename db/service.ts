9da77a99f8a1e913c9f74d5b1793230ef0605820
Kameq <90651206+kameqdev@users.noreply.github.com>
2026-02-10 18:28:53 +0100

    feat: add GuildTokensModel to manage guild tokens

--- db/service.ts
+++ db/service.ts
@@ -2,12 +2,14 @@ import { Database } from "@db/sqlite";
 import { ensureDirSync } from "@std/fs";
 import { ServerCodesModel } from "./models/serverCodes.ts";
 import { ConnectionsModel } from "./models/connections.ts";
+import { GuildTokensModel } from "./models/guildTokens.ts";
 
 class DatabaseService {
   private db: Database;
 
   public serverCodes: ServerCodesModel;
   public connections: ConnectionsModel;
+  public guildTokens: GuildTokensModel;
 
   constructor(dbPath = "./data/steamutils.db") {
     const dir = dbPath.substring(0, dbPath.lastIndexOf("/"));
@@ -17,6 +19,7 @@ class DatabaseService {
 
     this.serverCodes = new ServerCodesModel(this.db);
     this.connections = new ConnectionsModel(this.db);
+    this.guildTokens = new GuildTokensModel(this.db);
 
     this.seedData();
   }
