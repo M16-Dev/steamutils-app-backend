5ff09f38f24bf3858e4f307b716056f62c2cd104
Kameq <90651206+kameqdev@users.noreply.github.com>
2026-02-10 18:36:14 +0100

    refactor: extend ServerCodeRow interface from Server

--- db/types.ts
+++ db/types.ts
@@ -4,12 +4,9 @@ export interface Server {
   password?: string | null;
 }
 
-export interface ServerCodeRow {
+export interface ServerCodeRow extends Server {
   code: string;
   guild_id: string;
-  ip: string;
-  port: number;
-  password?: string | null;
   created_at: string;
 }
 
